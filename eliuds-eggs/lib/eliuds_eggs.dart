acfc6baf888afc7a7ac33c82a7bce212bc983793
Andrii Tseplyi <and.tseplyi@gmail.com>
2025-05-17 17:36:01 +0200

    Update eliuds_eggs.dart

--- eliuds-eggs/lib/eliuds_eggs.dart
+++ eliuds-eggs/lib/eliuds_eggs.dart
@@ -1,3 +1,10 @@
 class EggCounter {
-  // Your code goes here.
+  int count(int number) {
+    int binary = 0;
+    while (number != 0) {
+      binary += number % 2 == 0 ? 0 : 1;
+      number ~/= 2;
+    }
+    return binary;
+  }
 }
